841fd343d0d8bee9d12ec768fa8ae8ad5f861db2
A47181 <A47181@alunos.isel.pt>
2022-09-07 16:01:40 +0100

    First

---
+++ script.js
@@ -0,0 +1,42 @@
+let suits = ['Hearts', 'Diamonds','Clubs','Spades']
+let values = [2,3,4,5,6,7,8,9,10,'Jack','Queen','King','Ace']
+
+let deck = []
+
+let mainCard = ""
+let mainCardSuit = ""
+let mainCardValue = ""
+
+function fillDeck(){
+    for(let suit of suits){
+        for(let value of values){
+            deck.push(value + " of " + suit)
+        }
+    }
+}
+
+function shuffle(deck){
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+
+function pickACard(deck){
+    
+}
+
+function isNumber(card){
+    return('0' <= card[0] <= '9')
+}
+
+function isRed(card){
+    const suit = card.split(" ")[1]
+    return(suit[0] == 'H' || suit[0] == 'D')
+}
+
+function isHeart(card){
+    const suit = card.split(" ")[1]
+}

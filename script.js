83039767c697269d6d51c604d4bdf781a36f3973
A47181 <A47181@alunos.isel.pt>
2022-09-09 21:35:24 +0100

    Continued implementing game logic

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 let suits = ['Hearts', 'Diamonds','Clubs','Spades']
 let values = [2,3,4,5,6,7,8,9,10,'Jack','Queen','King','Ace']
 
-let deck = []
-
+let deck = [] 
 let mainCard = ""
 let mainCardSuit = ""
 let mainCardValue = ""
+const button1Elem = document.getElementById('btn1')
+const button2Elem = document.getElementById('btn2')
+
+startGame()
+console.log(mainCard)
+console.log(isHeart())
+
+button1Elem.addEventListener('click', () => {redGuessListener()})
+button2Elem.addEventListener('click',() => {blackGuessListener()})
 
 function fillDeck(){
+
     for(let suit of suits){
         for(let value of values){
             deck.push(value + " of " + suit)
@@ -16,33 +25,87 @@ function fillDeck(){
 }
 
 function shuffle(deck){
-    for (let i = array.length - 1; i > 0; i--) {
+    for (let i = deck.length - 1; i > 0; i--) {
         let j = Math.floor(Math.random() * (i + 1));
-        let temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
+        let temp = deck[i];
+        deck[i] = deck[j];
+        deck[j] = temp;
     }
 }
 
 function pickACard(deck){
-    //TODO add previous mainCard to history
-    mainCard = deck.pop
-    //TODO set main card image to actual card
+    mainCard = deck.pop()
+    mainCardSuit = mainCard.split(" ")[2]
+    mainCardValue = mainCard.split(" ")[0]
     
 }
 
-function isNumber(card){
-    return('0' <= card[0] <= '9')
+function redGuessListener(){
+    console.log("listener1")
+    if(!(isRed())){ 
+        endRound()
+        return
+    }
+    else guessedRedOrBlack()
 }
 
-function isRed(card){
-    const suit = card.split(" ")[1]
-    return(suit[0] == 'H' || suit[0] == 'D')
+function blackGuessListener(){
+    if(isRed()){ 
+        endRound()
+        return
+    }
+    else guessedRedOrBlack()
 }
 
-function isHeart(card){
-    const suit = card.split(" ")[1]
+function startGame(){
+    fillDeck()
+    shuffle(deck)
+    pickACard(deck)
+    
 }
 
-function numberGuess(){}
-function figureGuess(){}
+function restartGame(){
+    let deck = [] 
+    let mainCard = ""
+    let mainCardSuit = ""
+    let mainCardValue = ""
+    startGame()
+}
+
+function endRound(){
+    pickACard(deck)
+    //add previous card to history and show result (win or loss)
+}
+
+function isNumber(){
+    return('0' <= mainCardValue <= '9')
+}
+
+function isRed(){
+    return(mainCardSuit == 'Hearts' || mainCardSuit == 'Diamonds')
+} 
+
+function guessedRedOrBlack(){
+    button1Elem.removeEventListener('click',()=>{redGuessListener()},false)
+    button2Elem.removeEventListener('click',()=>{blackGuessListener()},false)
+    //change button text
+    button1Elem.addEventListener('click',()=>{guessedSuit()})
+    button2Elem.addEventListener('click',()=> guessedSuit())
+}
+
+function guessedSuit(){
+
+}
+
+function isHeart(){
+    return(mainCardSuit == 'Hearts')
+}
+
+function numberGuess(){
+    switch(mainCardValue){
+        case '1':
+    }
+}
+function figureGuess(card){}
+
+startGame()
